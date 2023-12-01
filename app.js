9115388334140c6b85062c301f0d7801aff7af02
shen-zx <152477420+shen-zx@users.noreply.github.com>
2023-11-30 22:45:15 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import Hello from "./hello.js";
+import Lab5 from './Lab5.js';
+import CourseRoutes from './courses/routes.js';
+import ModuleRoutes from './modules/routes.js';
+import cors from "cors";
+import "dotenv/config";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+Hello(app);
+ModuleRoutes(app);
+CourseRoutes(app);
+Lab5(app);
+app.listen(process.env.PORT || 4000);
+
